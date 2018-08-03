f6d5c668b10a778f44ef5bb80f8e2d13b7cf7076
Paul Harrington <paulh@ovationsgroup.com>
2018-08-03 10:12:58 +0200

    hero service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
-
+import { Hero } from './hero.model';
+import { HEROES } from './mock-heroes';
 @Injectable()
 export class HeroService {
 
   constructor() { }
-
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,22 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero } from '../Hero.model';
-import {HEROES} from '../mock-heroes';
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
 
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
+  getHeroes() {
+    this.heroes = this.heroService.getHeroes();
+  }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
